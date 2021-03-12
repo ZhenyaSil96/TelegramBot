aa7b31a103bdf6b90abab923c428878b2fc2600b
Yevhenii Silin <zhenya.sil96@gmail.com>
2021-03-12 19:11:35 +0200

    create bot

---
+++ index.js
@@ -0,0 +1,33 @@
+const TelegramBot = require('node-telegram-bot-api')
+const debug = require('./helpers')
+console.log('Bot has been started ...')
+
+const TOKEN = '1629315632:AAHgIslo_irJWSWVNVknMWJAjeHqtkCldes'
+const bot = new TelegramBot(TOKEN, {
+    polling: {
+        interval: 300,
+        autoStart: true,
+        params: {
+            timeout: 10
+        }
+    }
+})
+
+///Обработка сообщений
+// bot.on('message', (msg) => {
+//   console.log(msg)
+//   const {id} = msg.chat 
+
+//   if (msg.text.toLocaleLowerCase() === 'hello'){
+//     bot.sendMessage(id, `Hello ${msg.from.first_name}`)
+//   }else{
+//     bot.sendMessage(id,debug(msg))
+//     .then(()=> {
+//         console.log('Message has been send')
+//     })
+//     .catch((error) => {
+//       console.log(error)
+//     })
+//   }
+ 
+// })
